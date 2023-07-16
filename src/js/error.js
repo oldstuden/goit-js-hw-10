5c47059c8247f00ff7acd2684c64a2c9f900462e
oldstuden <atikin3002@gmail.com>
2023-07-16 21:30:35 +0200

    call function added to in error.js

--- src/js/error.js
+++ src/js/error.js
@@ -1,8 +1,10 @@
 import Notiflix from 'notiflix';
+import { showLoader } from './loader';
 
 function messageError() {
   Notiflix.Notify.failure(
     'Oops! Something went wrong! Try reloading the page!'
   );
+  showLoader('endSearch');
 }
 export { messageError };
